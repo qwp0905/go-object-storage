18635dac7e8604337a8fe3601d3625957bda5f24
qwp0905 <qwp0905@gmail.com>
2023-11-11 20:31:47 +0900

    성공하였다.... 드디어 된다...

---
+++ internal/nodepool/metadata.go
@@ -0,0 +1,91 @@
+package nodepool
+
+import (
+	"bytes"
+	"encoding/json"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/pkg/errors"
+	"github.com/qwp0905/go-object-storage/internal/datanode"
+	"github.com/valyala/fasthttp"
+)
+
+func (p *NodePool) getMetadata(host string, key string) (*datanode.Metadata, error) {
+	req := fasthttp.AcquireRequest()
+	defer fasthttp.ReleaseRequest(req)
+	res := fasthttp.AcquireResponse()
+	defer fasthttp.ReleaseResponse(res)
+
+	req.Header.SetMethod(fasthttp.MethodGet)
+	req.SetRequestURI(p.getMetaHost(host, key))
+	res.StreamBody = true
+
+	if err := p.client.Do(req, res); err != nil {
+		return nil, err
+	}
+
+	if res.StatusCode() == fiber.StatusNotFound {
+		return nil, fiber.ErrNotFound
+	} else if res.StatusCode() >= 400 {
+		return nil, errors.WithStack(errors.Errorf("%s", string(res.Body())))
+	}
+
+	data := new(datanode.Metadata)
+	if err := json.NewDecoder(res.BodyStream()).Decode(data); err != nil {
+		return nil, err
+	}
+
+	return data, nil
+}
+
+func (p *NodePool) putMetadata(host string, metadata *datanode.Metadata) error {
+	req := fasthttp.AcquireRequest()
+	defer fasthttp.ReleaseRequest(req)
+	res := fasthttp.AcquireResponse()
+	defer fasthttp.ReleaseResponse(res)
+
+	req.Header.SetMethod(fasthttp.MethodPut)
+	req.SetRequestURI(p.getMetaHost(host, ""))
+	req.Header.SetContentType("application/json")
+
+	b, err := json.Marshal(metadata)
+	if err != nil {
+		return errors.WithStack(err)
+	}
+	req.SetBodyStream(bytes.NewReader(b), len(b))
+
+	if err := p.client.Do(req, res); err != nil {
+		return err
+	}
+
+	if res.StatusCode() == fiber.StatusNotFound {
+		return fiber.ErrNotFound
+	} else if res.StatusCode() >= 400 {
+		return errors.WithStack(errors.Errorf("%s", string(res.Body())))
+	}
+
+	return nil
+}
+
+func (p *NodePool) deleteMetadata(host string, key string) error {
+	req := fasthttp.AcquireRequest()
+	defer fasthttp.ReleaseRequest(req)
+	res := fasthttp.AcquireResponse()
+	defer fasthttp.ReleaseResponse(res)
+
+	req.Header.SetMethod(fasthttp.MethodDelete)
+	req.SetRequestURI(p.getMetaHost(host, key))
+	res.StreamBody = true
+
+	if err := p.client.Do(req, res); err != nil {
+		return errors.WithStack(err)
+	}
+
+	if res.StatusCode() == fiber.StatusNotFound {
+		return fiber.ErrNotFound
+	} else if res.StatusCode() >= 400 {
+		return errors.WithStack(errors.Errorf("%s", string(res.Body())))
+	}
+
+	return nil
+}
