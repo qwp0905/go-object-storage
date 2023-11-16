6db567f397b816c3e4d9cd36e701ade34f8d2af0
qwp0905 <kwonjin@jentestore.com>
2023-11-16 13:59:07 +0900

    add rwlock

--- internal/locker/locker.go
+++ internal/locker/locker.go
@@ -1,3 +1,50 @@
 package locker
 
-//TODO rwlock 구현 필요할수도... 최소한 그냥 locker라도
+import (
+	"context"
+	"fmt"
+
+	"github.com/redis/go-redis/v9"
+)
+
+func readLockKey(key string) string {
+	return fmt.Sprintf("READ:%s", key)
+}
+
+func writeLockKey(key string) string {
+	return fmt.Sprintf("WRITE:%s", key)
+}
+
+const readLockScript = `
+if redis.call("EXISTS", KEYS[2]) then
+	return redis.call("PTTL", KEYS[2])
+end
+redis.call("RPUSH", KEYS[1], ARGV[1])
+redis.call("PTTL", ARGV[2])
+return -10
+`
+
+const readUnlockScript = `
+if redis.call("GET", KEYS[1])[0] ~= ARGV[1] then
+	return nil
+end
+`
+
+// TODO rwlock 구현 필요할수도... 최소한 그냥 locker라도
+type RWMutex struct {
+	rc      *redis.Client
+	key     string
+	current string
+}
+
+func NewRWMutex(rc *redis.Client, key string) *RWMutex {
+	return &RWMutex{rc: rc}
+}
+
+func (l *RWMutex) Lock(ctx context.Context) error {
+	sub := l.rc.Subscribe(ctx, l.key)
+	defer sub.Close()
+	for {
+		l.rc.Eval(ctx, acquireScript, []string{}).Int()
+	}
+}
