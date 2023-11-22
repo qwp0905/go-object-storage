3e4f61d7a2ae0e243239758c76e3749e18c12b81
qwp0905 <qwp0905@gmail.com>
2023-11-22 20:13:06 +0900

    add bloom filter

---
+++ internal/bloomfilter/bloomfilter.go
@@ -0,0 +1,77 @@
+package bloomfilter
+
+import (
+	"bytes"
+	"crypto/sha256"
+	"encoding/binary"
+	"sync"
+
+	"github.com/google/uuid"
+	"github.com/pkg/errors"
+)
+
+type BloomFilter struct {
+	bit           []bool
+	hashFunctions []func(string) (int, error)
+	lock          *sync.RWMutex
+}
+
+func createHashFunction(size int) func(string) (int, error) {
+	base := uuid.Must(uuid.NewRandom()).String()
+	return func(s string) (int, error) {
+		h := sha256.New()
+		if _, err := h.Write([]byte(s + base)); err != nil {
+			return 0, errors.WithStack(err)
+		}
+		n, err := binary.ReadUvarint(bytes.NewReader(h.Sum(nil)))
+		if err != nil {
+			return 0, errors.WithStack(err)
+		}
+
+		return int(n) % size, nil
+	}
+}
+
+func New(size, hash int) *BloomFilter {
+	hashFunctions := make([]func(string) (int, error), hash)
+	for i := range hashFunctions {
+		hashFunctions[i] = createHashFunction(size)
+	}
+	return &BloomFilter{
+		bit:           make([]bool, size),
+		hashFunctions: hashFunctions,
+		lock:          new(sync.RWMutex),
+	}
+}
+
+func (b *BloomFilter) Get(key string) (bool, error) {
+	b.lock.RLock()
+	defer b.lock.RUnlock()
+
+	for _, f := range b.hashFunctions {
+		n, err := f(key)
+		if err != nil {
+			return false, err
+		}
+		if !b.bit[n] {
+			return false, nil
+		}
+	}
+
+	return true, nil
+}
+
+func (b *BloomFilter) Set(key string) error {
+	b.lock.Lock()
+	defer b.lock.Unlock()
+
+	for _, f := range b.hashFunctions {
+		n, err := f(key)
+		if err != nil {
+			return err
+		}
+
+		b.bit[n] = true
+	}
+	return nil
+}
